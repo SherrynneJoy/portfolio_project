f7d99f0f6f9582213db0c7f5556e251d3b3736ea
SherrynneJoy <125467595+SherrynneJoy@users.noreply.github.com>
2024-05-26 21:23:42 +0300

    App.js

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,21 +1,35 @@
-import './App.css';
-import React, { useState, useEffect } from 'react';
+import React, {useState} from 'react';
 
-export default function App() {
-  const [lat, setLat] = useState([]);
-  const [long, setLong] = useState([]);
+function App() {
 
-useEffect(() => {
-	navigator.geolocation.getCurrentPosition(function(position) {
-		setLat(position.coords.latitude);
-		setLong(position.coords.longitude);
-	});
-	console.log("Latitude is:", lat)
-	console.log("Longitude is:", long)
-}, [lat, long]);
+// const url ='https://api.openweathermap.org/data/2.5/weather?lat=5&lon=42&appid=401186890c82b3946e39382041a4e4a1'
 
   return (
     <div className="App">
+	<div className="container">
+	  <div className='top'>
+	    <div className="location">
+	      <p>Nairobi</p>
+            </div>
+          <div className="temp">
+            <h1>19°C</h1>
+            <div className="description">
+              <p>Windy</p>
+            </div>
+          </div>
+        </div>
+        <div className="bottom">
+          <div className="feels">
+            <p>25°C</p>
+          </div>
+          <div className="humidity">
+            <p>87%</p>
+          </div>
+          <div className="wind">
+            <p>18 km/h</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
